635a5a1a16e1c85b3a340fb53efd06851230951f
Ava Collins <iamsimplesthing@gmail.com>
2023-02-19 22:35:05 -0800

    textures

--- celestial-equator/src/main.ts
+++ celestial-equator/src/main.ts
@@ -1,68 +1,96 @@
 import * as THREE from 'three'
 
-type Config = {
-  [key: string]: number
+const normal =  window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth
+const sizes = {
+    width: normal,
+    height: normal
 }
 
-const BOUNDS = {
-  width: 1000,
-  height: 1000
-}
 const LATITUDE = 47 // hardcoded here; to be derived from user GPS latitude rounded down
 const EQUATOR = LATITUDE + 90
-const CAMERA = { x: 0, y: 0, z: BOUNDS.width / 2 }
-const EARTH = { x: BOUNDS.width / 4, y: BOUNDS.height / 4 }
+const OBSERVER = { x: sizes.width / 4, y: sizes.height / 4 }
 const NORTH_STAR = { x: 0, y: 0 }
 
 function convertLatitude(lat) {
-  return lat * Math.PI / 180
+    return lat * Math.PI / 180
 }
 
-function rotate(angle) {
-  var origin: Config = EARTH
-  var coords: Config = NORTH_STAR
-  angle *= -1;
-
-  var x: number = (coords.x - origin.x) * Math.cos(angle) - (coords.y - origin.y) * Math.sin(angle);
-  var y: number = (coords.x - origin.x) * Math.sin(angle) + (coords.y - origin.y) * Math.cos(angle);
-  return { "x": x + origin.x * 1, "y": y + origin.y * 1}
+function rotate(angle, origin, coords) {
+    angle *= -1;
+    var x: number = (coords.x - origin.x) * Math.cos(angle) - (coords.y - origin.y) * Math.sin(angle);
+    var y: number = (coords.x - origin.x) * Math.sin(angle) + (coords.y - origin.y) * Math.cos(angle);
+    return { "x": x + origin.x * 1, "y": y + origin.y * 1 }
 }
 
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
-const earthGeometry = new THREE.SphereGeometry(60, 32, 16);
-const earthMaterial = new THREE.MeshBasicMaterial({ color: 0x333cff });
+
+const loadingManager = new THREE.LoadingManager()
+const textureLoader = new THREE.TextureLoader(loadingManager)
+
+const earthTexture = textureLoader.load(
+    '/textures/2k_earth_daymap.jpg'
+)
+
+const zodiacAlpha = textureLoader.load(
+    '/textures/zodiac_alpha.png'
+)
+
+const earthGeometry = new THREE.SphereGeometry(.5, 32, 16);
+const earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture })
 const earthSphere = new THREE.Mesh(earthGeometry, earthMaterial);
 scene.add(earthSphere);
 
-
-const northStarGeometry = new THREE.OctahedronGeometry(8, 0);
-const northStarMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff});
+const northStarGeometry = new THREE.OctahedronGeometry(.05, 0);
+const northStarMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
 const northStarSphere = new THREE.Mesh(northStarGeometry, northStarMaterial);
-const northStarPosition = rotate(convertLatitude(LATITUDE))
-northStarSphere.position.x = northStarPosition.x
-northStarSphere.position.y = northStarPosition.y
+const northStarPosition = rotate(convertLatitude(LATITUDE), OBSERVER, NORTH_STAR)
+northStarSphere.position.x = northStarPosition.x /100
+northStarSphere.position.y = northStarPosition.y /100
 scene.add(northStarSphere)
 
-const celestialEqGeometry = new THREE.CylinderGeometry(100, 100, 50, 32);
-const celestialEqMaterial = new THREE.MeshBasicMaterial({ color: 0x0ff000 });
+const celestialEqGeometry = new THREE.CylinderGeometry(1, 1, .65, 32);
+const celestialEqMaterial = new THREE.MeshBasicMaterial();
+celestialEqMaterial.transparent = true
+celestialEqMaterial.alphaMap = zodiacAlpha
 const celestialEqMesh = new THREE.Mesh(celestialEqGeometry, celestialEqMaterial);
-const celestialEqPosition = rotate(convertLatitude(EQUATOR))
-celestialEqMesh.rotation.z = Math.floor(celestialEqPosition.x) // angle of equator from latitude
+const celestialEqPosition = rotate(convertLatitude(EQUATOR), OBSERVER, NORTH_STAR)
+celestialEqMesh.rotation.z = Math.floor(celestialEqPosition.x/100) // angle of equator from latitude
 scene.add(celestialEqMesh)
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, BOUNDS.width / BOUNDS.height)
-camera.position.z = CAMERA.z
-camera.position.y = CAMERA.y
+window.addEventListener('resize', () => {
+    var norm = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth
+    sizes.width = norm
+    sizes.height = norm
+
+    camera.aspect = sizes.width / sizes.height
+ 
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = .25
+camera.position.z = (sizes.height/2.5)/100
 scene.add(camera)
 
-// Renderer
+
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+    canvas: canvas
 })
-renderer.setSize(BOUNDS.width, BOUNDS.height)
-renderer.render(scene, camera)
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+const tick = () => {
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
